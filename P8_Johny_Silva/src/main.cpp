b86113ea44385ef9db72194dda8e2d68586b5bbe
Johny Silva Mendes <101329829+JohnyMendes@users.noreply.github.com>
2022-06-02 23:36:12 +0200

    Add files via upload

---
+++ P8_Johny_Silva/src/main.cpp
@@ -0,0 +1,19 @@
+// Johny Silva Mendes
+// PRACTICA 8: Buses de Comunicación IV (UART)
+#include <Arduino.h>
+
+void setup() {
+  Serial.begin(9600);
+  Serial2.begin(9600);
+  Serial.write("Inicializando...");
+}
+
+void loop() {
+if(Serial.available()){
+  Serial2.write(Serial.read());
+  delay(10);
+  if(Serial2.available()){
+    Serial.write(Serial2.read());
+  }
+}
+}
